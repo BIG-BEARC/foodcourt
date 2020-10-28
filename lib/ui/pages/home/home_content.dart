f1f9423be0014e25c9bfc8945b1337f698764734
chuxiong <chuxiong@xgd.com>
2020-10-28 14:57:04 +0800

    refactor:重构 home_content.dart 代码，采用 FutureBuild + StatelessWidget 替换 StatefulWidget

--- lib/ui/pages/home/home_content.dart
+++ lib/ui/pages/home/home_content.dart
@@ -4,41 +4,39 @@ import 'package:foodcourt/core/services/json_parse.dart';
 import 'package:foodcourt/core/extension/int_extension.dart';
 
 import 'home_content_item.dart';
-class FCHomeContent extends StatefulWidget {
-  @override
-  _FCHomeContentState createState() => _FCHomeContentState();
-}
 
-class _FCHomeContentState extends State<FCHomeContent> {
-  List<CategoryModel> _categoryModels = [];
-
-  @override
-  void initState() {
-    super.initState();
-    JsonParse.getCategoryJsonParseData().then((value) {
-      setState(() {
-        _categoryModels = value;
-      });
-    });
-  }
-
+class FCHomeContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      child: GridView.builder(
-        padding: EdgeInsets.all(10.px),
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          childAspectRatio: 1.5,
-          crossAxisCount: 2,
-          crossAxisSpacing: 20.px,
-          mainAxisSpacing: 20.px,
-        ),
-        itemCount: _categoryModels.length,
-        itemBuilder: (ctx, index) {
-          return FCHomeContentItem(_categoryModels[index]);
-        },
-      ),
-    );
+    return FutureBuilder<List<CategoryModel>>(
+        future: JsonParse.getCategoryJsonParseData(),
+        builder: (ctx, snapshot) {
+          if (!snapshot.hasData) {
+            return Center(child: CircularProgressIndicator());
+          }
+          if (snapshot.hasError)
+            return Center(
+              child: Text('加载失败!'),
+            );
+
+          var categoryModels = snapshot.data;
+
+          return Container(
+            alignment: Alignment.center,
+            child: GridView.builder(
+              padding: EdgeInsets.all(10.px),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                childAspectRatio: 1.5,
+                crossAxisCount: 2,
+                crossAxisSpacing: 20.px,
+                mainAxisSpacing: 20.px,
+              ),
+              itemCount: categoryModels.length,
+              itemBuilder: (ctx, index) {
+                return FCHomeContentItem(categoryModels[index]);
+              },
+            ),
+          );
+        });
   }
 }
